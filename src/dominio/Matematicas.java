09b8f0416cb56cfad864645b8b406c8174912a59
Pedro <pedrojalvoperezlozao@gmail.com>
2024-03-07 19:22:51 +0100

    Acabado

--- src/dominio/Matematicas.java
+++ src/dominio/Matematicas.java
@@ -5,13 +5,13 @@ import java.util.stream.IntStream;
 
 public class Matematicas {
     public static double generarNumeroPiIterativo(long pasos) {
-        Random random = new Random(); // Creamos una sola instancia de Random
+        Random random = new Random(); 
         int aciertos = (int) IntStream.range(0, (int) pasos)
                 .mapToObj(i -> new double[]{random.nextDouble(), random.nextDouble()})
                 .mapToDouble(xy -> Math.sqrt(xy[0] * xy[0] + xy[1] * xy[1]) <= 1.0 ? 1 : 0)
                 .sum();
 
         double areaCuadrado = 4.0;
-        return areaCuadrado * (aciertos / (double) pasos); // Convertimos pasos a double para obtener la división correcta
+        return areaCuadrado * (aciertos / (double) pasos);
     }
 }
